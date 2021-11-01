95583bd0beadeb314c7062f9cac8318db55dc5ee
hoangta <blue.hoangta@gmail.com>
2021-11-01 23:42:14 +0700

    a

--- MoveRX/Extensions/UIImage.swift
+++ MoveRX/Extensions/UIImage.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 extension UIImage {
-    static let close = UIImage(named: "icon-close")!
-    static let home = UIImage(named: "icon-home")!
-    static let delivery = UIImage(named: "icon-delivery")!
-    static let chat = UIImage(named: "icon-chat")!
-    static let search = UIImage(named: "icon-search")!
+    static let close = UIImage(named: "icon-close", in: .moveRx, compatibleWith: nil)!
+    static let home = UIImage(named: "icon-home", in: .moveRx, compatibleWith: nil)!
+    static let delivery = UIImage(named: "icon-delivery", in: .moveRx, compatibleWith: nil)!
+    static let chat = UIImage(named: "icon-chat", in: .moveRx, compatibleWith: nil)!
+    static let search = UIImage(named: "icon-search", in: .moveRx, compatibleWith: nil)!
+}
+
+extension Bundle {
+    static let moveRx = Bundle(for: MoveRx.self)
 }
